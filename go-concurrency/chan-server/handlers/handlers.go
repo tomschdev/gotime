081737a99c04ce456f70d6a6ba79a9744efc9408
Thomas Scholtz <tomsch@Thomass-MacBook-Air.local>
2022-08-06 11:22:18 +0200

    chan-server final version

--- go-concurrency/chan-server/handlers/handlers.go
+++ go-concurrency/chan-server/handlers/handlers.go
@@ -1,24 +1,32 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/applied-concurrency-in-go/models"
 	"github.com/applied-concurrency-in-go/repo"
 	"github.com/gorilla/mux"
 )
 
+// handler holds all the dependencies required for server requests
 type handler struct {
 	repo repo.Repo
+	once sync.Once
 }
-
+// Handler is the interface we expose to outside packages
 type Handler interface {
 	Index(w http.ResponseWriter, r *http.Request)
 	ProductIndex(w http.ResponseWriter, r *http.Request)
 	OrderShow(w http.ResponseWriter, r *http.Request)
 	OrderInsert(w http.ResponseWriter, r *http.Request)
+	Close(w http.ResponseWriter, r *http.Request)
+	Stats(w http.ResponseWriter, r *http.Request)
+	OrderReverse(w http.ResponseWriter, r *http.Request)
 }
 
 func New() (Handler, error) {
@@ -26,11 +34,13 @@ func New() (Handler, error) {
 	if err != nil {
 		return nil, err
 	}
-	h := handler{repo: r}
+	h := handler{
+		repo: r,
+	}
 	return &h, nil
 }
 
-// Index returns a simple hello response for the homepage
+// Index returns a simple welcome response for the homepage
 func (h *handler) Index(w http.ResponseWriter, r *http.Request) {
 	// Send an HTTP status & a hardcoded message
 	writeResponse(w, http.StatusOK, "Welcome to the Orders App!", nil)
@@ -38,12 +48,11 @@ func (h *handler) Index(w http.ResponseWriter, r *http.Request) {
 
 // ProductIndex displays all products in the system
 func (h *handler) ProductIndex(w http.ResponseWriter, r *http.Request) {
-	p := h.repo.GetAllProducts()
 	// Send an HTTP status & send the slice
-	writeResponse(w, http.StatusOK, p, nil)
+	writeResponse(w, http.StatusOK, h.repo.GetAllProducts(), nil)
 }
 
-// OrderShow fetches and displays one selected product
+// OrderShow fetches and displays one existing order
 func (h *handler) OrderShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	orderId := vars["orderId"]
@@ -71,6 +80,43 @@ func (h *handler) OrderInsert(w http.ResponseWriter, r *http.Request) {
 		writeResponse(w, http.StatusInternalServerError, nil, err)
 		return
 	}
+	writeResponse(w, http.StatusOK, order, nil)
+}
+
+// Close closes the orders app for new orders
+func (h *handler) Close(w http.ResponseWriter, r *http.Request) {
+	h.invokeClose()
+	writeResponse(w, http.StatusOK, "The Orders App is now closed!", nil)
+}
+
+// Stats outputs order statistics from the repo
+func (h *handler) Stats(w http.ResponseWriter, r *http.Request) {
+	reqCtx := r.Context()
+	ctx, cancel := context.WithTimeout(reqCtx, 600*time.Millisecond)
+	defer cancel()
+	stats, err := h.repo.GetOrderStats(ctx)
+	if err != nil {
+		writeResponse(w, http.StatusInternalServerError, nil, err)
+		return
+	}
+	writeResponse(w, http.StatusOK, stats, nil)
+}
+
+// OrderReverse fetches and displays one selected product
+func (h *handler) OrderReverse(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	orderId := vars["orderId"]
+	order, err := h.repo.RequestReversal(orderId)
+	if err != nil {
+		writeResponse(w, http.StatusInternalServerError, nil, err)
+		return
+	}
 
 	writeResponse(w, http.StatusOK, order, nil)
+}
+
+func (h *handler) invokeClose() {
+	h.once.Do(func() {
+		h.repo.Close()
+	})
 }
--- go-concurrency/chan-server/repo/repo.go
+++ go-concurrency/chan-server/repo/repo.go
@@ -1,18 +1,23 @@
 package repo
 
 import (
+	"context"
 	"fmt"
 	"math"
 
 	"github.com/applied-concurrency-in-go/db"
 	"github.com/applied-concurrency-in-go/models"
+	"github.com/applied-concurrency-in-go/stats"
 )
 
 // repo holds all the dependencies required for repo operations
 type repo struct {
-	products *db.ProductDB
-	orders   *db.OrderDB
-	incoming chan models.Order
+	products  *db.ProductDB
+	orders    *db.OrderDB
+	stats     stats.StatsService
+	incoming  chan models.Order
+	done      chan struct{}
+	processed chan models.Order
 }
 
 // Repo is the interface we expose to outside packages
@@ -20,23 +25,31 @@ type Repo interface {
 	CreateOrder(item models.Item) (*models.Order, error)
 	GetAllProducts() []models.Product
 	GetOrder(id string) (models.Order, error)
+	Close()
+	GetOrderStats(ctx context.Context) (models.Statistics, error)
+	RequestReversal(orderId string) (*models.Order, error)
 }
 
 // New creates a new Order repo with the correct database dependencies
 func New() (Repo, error) {
+	processed := make(chan models.Order, stats.WorkerCount)
+	done := make(chan struct{})
 	p, err := db.NewProducts()
 	if err != nil {
 		return nil, err
 	}
+	statsService := stats.New(processed, done)
 	o := repo{
-		products: p,
-		orders:   db.NewOrders(),
-		incoming: make(chan models.Order),
+		products:  p,
+		orders:    db.NewOrders(),
+		stats:     statsService,
+		incoming:  make(chan models.Order),
+		done:      done,
+		processed: processed,
 	}
 
 	// start the order processor
 	go o.processOrders()
-
 	return &o, nil
 }
 
@@ -56,11 +69,14 @@ func (r *repo) CreateOrder(item models.Item) (*models.Order, error) {
 		return nil, err
 	}
 	order := models.NewOrder(item)
-	r.orders.Upsert(order)
-
 	// place the order on the incoming orders channel
-	r.incoming <- order
-	return &order, nil
+	select {
+	case r.incoming <- order:
+		r.orders.Upsert(order)
+		return &order, nil
+	case <-r.done:
+		return nil, fmt.Errorf("orders app is closed, try again later")
+	}
 }
 
 // validateItem runs validations on a given order
@@ -76,17 +92,31 @@ func (r *repo) validateItem(item models.Item) error {
 
 func (r *repo) processOrders() {
 	fmt.Println("Order processing started!")
-	for order := range r.incoming {
-		r.processOrder(&order)
-		r.orders.Upsert(order)
-		fmt.Printf("Processing order %s completed\n", order.ID)
+	for {
+		select {
+		case order := <-r.incoming:
+			r.processOrder(&order)
+			r.orders.Upsert(order)
+			fmt.Printf("Processing order %s completed\n", order.ID)
+			r.processed <- order
+		case <-r.done:
+			fmt.Println("Order processing stopped!")
+			return
+		}
 	}
-	fmt.Println("Order processing stopped!")
 }
 
 // processOrder is an internal method which completes or rejects an order
 func (r *repo) processOrder(order *models.Order) {
+	// ensure the order is still completed
+	fetchedOrder, err := r.orders.Find(order.ID)
+	if err != nil || fetchedOrder.Status != models.OrderStatus_Completed {
+		fmt.Println("duplicate reversal on order ", order.ID)
+	}
 	item := order.Item
+	if order.Status == models.OrderStatus_ReversalRequested {
+		item.Amount = -item.Amount
+	}
 	product, err := r.products.Find(item.ProductID)
 	if err != nil {
 		order.Status = models.OrderStatus_Rejected
@@ -105,4 +135,41 @@ func (r *repo) processOrder(order *models.Order) {
 	total := math.Round(float64(order.Item.Amount)*product.Price*100) / 100
 	order.Total = &total
 	order.Complete()
+}
+
+// Close closes the orders app for incoming orders
+func (r *repo) Close() {
+	close(r.done)
+}
+
+// GetOrderStats returns the order statistics of the orders app
+func (r repo) GetOrderStats(ctx context.Context) (models.Statistics, error) {
+	select {
+	case s := <-r.stats.GetStats(ctx):
+		return s, nil
+	case <-ctx.Done():
+		return models.Statistics{}, ctx.Err()
+	}
+}
+
+// RequestReversal fetches an existing order and updates it for reversal
+func (r repo) RequestReversal(orderId string) (*models.Order, error) {
+	// try to find the order first
+	order, err := r.orders.Find(orderId)
+	if err != nil {
+		return nil, err
+	}
+	if order.Status != models.OrderStatus_Completed {
+		return nil, fmt.Errorf("order status is %s, only completed orders can be requested for reversal", order.Status)
+	}
+	// set reversal requested
+	order.Status = models.OrderStatus_ReversalRequested
+	// place the order on the incoming orders channel
+	select {
+	case r.incoming <- order:
+		r.orders.Upsert(order)
+		return &order, nil
+	case <-r.done:
+		return nil, fmt.Errorf("sorry, the orders app is closed")
+	}
 }
